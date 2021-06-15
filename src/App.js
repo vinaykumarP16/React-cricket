f8ee4dbe8132b0fb112136cdab0eb80ec3a4736b
vinaykumarP16 <vinaykumar.p1995v@gmail.com>
2021-06-15 18:05:17 +0530

    first done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Bat from './Bat.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1 style={{color:'orange',margin:'20px'}}>Play - cricket</h1>
+      <Bat/>
+     </div>
   );
 }
 
